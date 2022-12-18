f8ea868f008dbbd9217e5a4b8237fae986e9660f
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2022-12-18 09:40:43 +0530

    Final Commmit

--- src/App.js
+++ src/App.js
@@ -32,12 +32,10 @@
 import { ChatEngine } from "react-chat-engine";
 import './App.css';
 import Chatfeed from './components/ChatFeed'
-import LoginForm from "./components/LoginForm";
 
 const projectID = ''
 const App = () =>{
 
-   // if(!localStorage.getItem('username'))return <LoginForm />
 
 
 
@@ -46,7 +44,7 @@ const App = () =>{
         <ChatEngine 
             height="100vh"
             projectID="912b0ecc-0bb2-4aab-82f5-b18c29bda022"
-            userName="Punk"
+            userName="Admin"
             userSecret="12345"
             renderChatFeed={(chatAppProps) =>< Chatfeed {...chatAppProps}/>}
         />
